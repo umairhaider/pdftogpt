1dc6c08636dab88d488043ea607375408b8d8d81
Umair Haider <umrhdr@gmail.com>
2023-06-14 18:33:19 +0300

    🧪 test - fix issues in openai key import

--- app/utils/langchain_utils.py
+++ app/utils/langchain_utils.py
@@ -1,5 +1,4 @@
 import logging
-import openai
 import os
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -9,17 +8,12 @@ from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 from app.api.knowledgebase_handler import get_knowledge_base, set_knowledge_base
 
-# Configure OpenAI API credentials
-def set_openai_key():
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
 # If the API key is not set, raise an exception
-if openai.api_key is None:
+if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
     raise Exception("Please set your OPENAI_API_KEY as an environment variable.")
 
 def process_file_context(file_text):
     try:
-        set_openai_key()
         # split into chunks
         text_splitter = CharacterTextSplitter(
             separator="\n",
@@ -39,7 +33,6 @@ def process_file_context(file_text):
 
 def process_user_question(user_question):
     try:
-        set_openai_key()
         knowledge_base = get_knowledge_base()
         
         # show user input
